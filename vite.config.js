571d997b91e4f28f688b074b5e7666e6a57037a2
Patan AbdullaKhan <abdullakhan8999@gmail.com>
2023-08-28 16:32:56 +0530

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 let BASE_URL = 'https://ecomm-backend-5fix.onrender.com';
-BASE_URL = "http://localhost:4000"
+// BASE_URL = "http://localhost:4000"
 
 export default defineConfig({
   server: {
